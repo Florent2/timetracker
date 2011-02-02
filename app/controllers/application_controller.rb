419aa746b1f6a99b45ded89088d9be4f587456df
Florent Guilleux <florent2@gmail.com>
2011-02-02 14:31:07 -0500

    Display interrupted in sidebar, and reduce logic code in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :get_projects
+  before_filter :get_running_task
+  before_filter :get_interrupted_tasks
   
   private
   
+  def get_interrupted_tasks
+    @interrupted_tasks = Task.actives - [@running_task]
+  end
+  
   def get_projects
     @projects = Project.recents_first
+  end
+  
+  def get_running_task
+    @running_task = Task.running_task    
   end
 end
