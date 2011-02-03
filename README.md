8773836dcac19a165dfa85458e8a20a708181814
Florent Guilleux <florent2@gmail.com>
2011-02-03 00:25:32 -0500

    Improve web design

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Personal timetracker
 ====================
 
-Requires Ruby 1.9.x to have tasks ordered by days in chronological order.
+Requires Ruby 1.9.x to have tasks ordered by days in chronological order. Web design only tested in Chrome.
 
 A task can be either:
-  * running (there can be only one running task): it can be interrupted or archived
-  * interrupted: it can be resumed or archived
-  * archived: it can be resumed
+
+  * running:      it can be interrupted or archived; there can be only one running task
+  * interrupted:  it can be resumed or archived
+  * archived:     it can be resumed
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,22 @@
 module ApplicationHelper
 
   def archive_link(task, options={})
-    options.reverse_merge! :method => :put
-    link_to "archive", task_archive_path(task), options
+    options.reverse_merge! :method => :put, :class => "archive action", :title => "archive", :name => "archive"
+    link_to options[:name], task_archive_path(task), options
   end
   
   def interrupt_link(task, options={})
-    options.reverse_merge! :method => :put
-    link_to "interrupt", task_interrupt_path(task), options
+    options.reverse_merge! :method => :put, :class => "interrupt action", :title => "interrupt", :name => "interrupt"
+    link_to options[:name], task_interrupt_path(task), options
   end
 
   def resume_link(task, options={})
-    options.reverse_merge! :method => :put    
-    link_to "resume", task_resume_path(task), options
+    options.reverse_merge! :method => :put, :class => "resume action", :title => "resume", :name => "resume"    
+    link_to options[:name], task_resume_path(task), options
+  end
+  
+  def status_span(task)
+    content_tag :span, task.status, :class => task.status
   end
 
 end
