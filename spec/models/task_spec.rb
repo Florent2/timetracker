f9dd2ace9f1db4293b13d6e58260755a8d976e64
Florent Guilleux <florent2@gmail.com>
2011-02-03 13:00:59 -0500

    Complete and reorder Task specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -36,6 +36,104 @@ describe Task do
       end
     end
   end
+
+  it "new running task interrupts previous running task" do
+    running_task = Fabricate :running_task
+    new_task = Fabricate :running_task
+    running_task.running?.should be_false
+    new_task.running?.should be_true
+  end
+  
+  describe "#archive!" do
+    it "returns true and set archived to true if task was interrupted" do
+      task = Fabricate :interrupted_task
+      task.archive!.should be_true
+      task.archived?.should be_true
+    end
+    it "returns true, set archived to true and interrupt last session if task was running" do
+      task = Fabricate :running_task
+      task.archive!
+      task.archived?.should be_true
+      task.running?.should be_false
+    end
+    it "returns false if task was already archived" do
+      task = Fabricate :archived_task
+      task.archive!.should be_false
+    end
+  end
+  
+  it ".by_dates_from(tasks, date) returns an hash whose keys are dates and values are array of tasks having sessions on the key date" do
+    task1 = Fabricate :task # 10 days ago
+    task1.sessions << Fabricate(:session, :task => task1, :start => DateTime.current.advance(:days => -10), :finish => DateTime.current.advance(:days => -10, :hours => +2))
+
+    task2 = Fabricate :task # 4 and 2 days ago
+    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -4), :finish => DateTime.current.advance(:days => -4, :hours => +2))
+    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -2), :finish => DateTime.current.advance(:days => -2, :hours => +2))
+    
+    task3 = Fabricate :task # today and 2 days ago
+    task3.sessions << Fabricate(:session, :task => task3, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +2))
+    task3.sessions << Fabricate(:session, :task => task3, :start => DateTime.current.advance(:days => -2), :finish => DateTime.current.advance(:days => -2, :hours => +2))
+    
+    Task.by_dates_from(Task, Date.today.advance(:days => -5)).should == {
+      Date.today.advance(:days => -4) => [task2],
+      Date.today.advance(:days => -2) => [task2, task3],
+      Date.today  => [task3]
+    }
+  end
+  
+  describe "#duration_current" do
+    it "returns 0.0 if the task is interrupted" do
+      Fabricate(:interrupted_task).duration_current.should == 0.0
+    end
+    it "returns the duration of the running session if the task is running" do
+      task = Fabricate(:task)
+      running_session = Fabricate :session, :task => task, :start => DateTime.current.advance(:hours => -1)
+      task.duration_current.should == 1.0
+    end
+  end
+  
+  it ".duration_from(tasks, date) returns the duration of the given tasks from the given date" do
+    task1 = Fabricate :task
+    task1.sessions << Fabricate(:session, :task => task1, :start => DateTime.current.advance(:days => -2), :finish => DateTime.current.advance(:days => -2, :hours => +10)) # 10 hours 2 days ago
+
+    task2 = Fabricate :task
+    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -4), :finish => DateTime.current.advance(:days => -4, :hours => +2)) # 2 hours 4 days ago
+    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -2), :finish => DateTime.current.advance(:days => -2, :hours => +4)) # 4 hours 2 days ago
+
+    task3 = Fabricate :task
+    task3.sessions << Fabricate(:session, :task => task3, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +2)) # 2 hours today
+
+    Task.duration_from([task2, task3], Date.today.advance(:days => -2)).should == 6.0
+  end
+  
+  describe "#duration_on_date(date)" do
+    it "returns 0.00 if there was no session on this date" do
+      task = Fabricate :task
+      task.duration_on_date(Date.yesterday).should == 0.00
+    end
+    it "returns the duration of sessions of this date" do
+      task = Fabricate :task
+      task.sessions << Fabricate(:session, :task => task, :start => DateTime.current.advance(:days => -1), :finish => DateTime.current.advance(:days => -1, :hours => +2)) # 2 hours   
+      task.sessions << Fabricate(:session, :task => task, :start => DateTime.current.advance(:days => -1, :hours => +2), :finish => DateTime.current.advance(:days => -1, :hours => +5.5)) # 3 hours   
+      task.sessions << Fabricate(:session, :task => task, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +2.5)) # 2.5 hours   
+      task.duration_on_date(Date.yesterday).should == 5.50
+      task.duration_on_date(Date.today).should == 2.50      
+    end
+  end
+  
+  it ".duration_on_date(tasks, date) returns the duration of the given tasks on the given date" do
+    task1 = Fabricate :task
+    task1.sessions << Fabricate(:session, :task => task1, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +10)) # 10 hours today
+
+    task2 = Fabricate :task
+    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -4), :finish => DateTime.current.advance(:days => -4, :hours => +2)) # 2 hours 4 days ago
+    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +4)) # 4 hours today
+
+    task3 = Fabricate :task
+    task3.sessions << Fabricate(:session, :task => task3, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +2)) # 2 hours today
+
+    Task.duration_on_date([task2, task3], Date.today).should == 6.0
+  end
   
   describe "#duration_total returns task duration in hours" do
     it "returns 0 if task has no sessions" do
@@ -51,23 +149,6 @@ describe Task do
       Fabricate :session, :task => task, :start => DateTime.current.advance(:hours => +1), :finish => DateTime.current.advance(:hours => +4) # 3 hours
       Fabricate :session, :task => task, :start => DateTime.current.advance(:hours => +4), :finish => DateTime.current.advance(:hours => +6) # 2 hours
       task.duration_total.should == 7.0
-    end
-  end
-  
-  describe "#running?" do
-    let(:task) { Fabricate :task }
-    it "returns true if there is an unfinished session" do
-      Fabricate :session, :task => task, :finish => DateTime.current.advance(:hours => 4)
-      Fabricate :session, :task => task
-      task.running?.should be_true
-    end
-    it "returns false if there are only finished sessions" do
-      Fabricate :session, :task => task, :finish => DateTime.current.advance(:hours => 4)
-      Fabricate :session, :task => task, :finish => DateTime.current.advance(:hours => 4)
-      task.running?.should be_false
-    end
-    it "returns false if there is no session" do
-      task.running?.should be_false
     end
   end
   
@@ -109,14 +190,20 @@ describe Task do
     end
   end
   
-  describe "#duration_current" do
-    it "returns 0.0 if the task is interrupted" do
-      Fabricate(:interrupted_task).duration_current.should == 0.0
+  describe "#running?" do
+    let(:task) { Fabricate :task }
+    it "returns true if there is an unfinished session" do
+      Fabricate :session, :task => task, :finish => DateTime.current.advance(:hours => 4)
+      Fabricate :session, :task => task
+      task.running?.should be_true
     end
-    it "returns the duration of the running session if the task is running" do
-      task = Fabricate(:task)
-      running_session = Fabricate :session, :task => task, :start => DateTime.current.advance(:hours => -1)
-      task.duration_current.should == 1.0
+    it "returns false if there are only finished sessions" do
+      Fabricate :session, :task => task, :finish => DateTime.current.advance(:hours => 4)
+      Fabricate :session, :task => task, :finish => DateTime.current.advance(:hours => 4)
+      task.running?.should be_false
+    end
+    it "returns false if there is no session" do
+      task.running?.should be_false
     end
   end
   
@@ -130,66 +217,8 @@ describe Task do
       2.times { Fabricate :interrupted_task }
       Task.running_task.should be_nil
     end
-  end
+  end  
   
-  it "new running task interrupts previous running task" do
-    running_task = Fabricate :running_task
-    new_task = Fabricate :running_task
-    running_task.running?.should be_false
-    new_task.running?.should be_true
-  end
-  
-  describe "#duration_on_date(date)" do
-    it "returns 0.00 if there was no session on this date" do
-      task = Fabricate :task
-      task.duration_on_date(Date.yesterday).should == 0.00
-    end
-    it "returns the duration of sessions of this date" do
-      task = Fabricate :task
-      task.sessions << Fabricate(:session, :task => task, :start => DateTime.current.advance(:days => -1), :finish => DateTime.current.advance(:days => -1, :hours => +2)) # 2 hours   
-      task.sessions << Fabricate(:session, :task => task, :start => DateTime.current.advance(:days => -1, :hours => +2), :finish => DateTime.current.advance(:days => -1, :hours => +5.5)) # 3 hours   
-      task.sessions << Fabricate(:session, :task => task, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +2.5)) # 2.5 hours   
-      task.duration_on_date(Date.yesterday).should == 5.50
-      task.duration_on_date(Date.today).should == 2.50      
-    end
-  end
-  
-  it ".by_dates_from(tasks, date) returns an hash whose keys are dates and values are array of tasks having sessions on the key date" do
-    task1 = Fabricate :task # 10 days ago
-    task1.sessions << Fabricate(:session, :task => task1, :start => DateTime.current.advance(:days => -10), :finish => DateTime.current.advance(:days => -10, :hours => +2))
-
-    task2 = Fabricate :task # 4 and 2 days ago
-    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -4), :finish => DateTime.current.advance(:days => -4, :hours => +2))
-    task2.sessions << Fabricate(:session, :task => task2, :start => DateTime.current.advance(:days => -2), :finish => DateTime.current.advance(:days => -2, :hours => +2))
-    
-    task3 = Fabricate :task # today and 2 days ago
-    task3.sessions << Fabricate(:session, :task => task3, :start => DateTime.current, :finish => DateTime.current.advance(:hours => +2))
-    task3.sessions << Fabricate(:session, :task => task3, :start => DateTime.current.advance(:days => -2), :finish => DateTime.current.advance(:days => -2, :hours => +2))
-    
-    Task.by_dates_from(Task, Date.today.advance(:days => -5)).should == {
-      Date.today.advance(:days => -4) => [task2],
-      Date.today.advance(:days => -2) => [task2, task3],
-      Date.today  => [task3]
-    }
-  end
-  
-  describe "#archive!" do
-    it "returns true and set archived to true if task was interrupted" do
-      task = Fabricate :interrupted_task
-      task.archive!.should be_true
-      task.archived?.should be_true
-    end
-    it "returns true, set archived to true and interrupt last session if task was running" do
-      task = Fabricate :running_task
-      task.archive!
-      task.archived?.should be_true
-      task.running?.should be_false
-    end
-    it "returns false if task was already archived" do
-      task = Fabricate :archived_task
-      task.archive!.should be_false
-    end
-  end
 end
 
 
