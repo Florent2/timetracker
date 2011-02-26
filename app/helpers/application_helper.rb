7b461f95d3a26a4ce8bd66146597405d917722c5
Florent Guilleux <florent2@gmail.com>
2011-02-26 18:08:02 -0500

    Add accesskeys for common actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,19 @@ module ApplicationHelper
 
   def archive_link(task, options={})
     options.reverse_merge! :method => :put, :class => "archive action", :title => "archive", :name => "archive"
+    add_access_key_to_title options
     link_to options[:name], task_archive_path(task), options
   end
   
   def interrupt_link(task, options={})
-    options.reverse_merge! :method => :put, :class => "interrupt action", :title => "interrupt", :name => "interrupt"
+    options.reverse_merge! :method => :put, :class => "interrupt action", :title => "interrupt", :name => "interrupt", :accesskey => "i"
+    add_access_key_to_title options    
     link_to options[:name], task_interrupt_path(task), options
   end
 
   def resume_link(task, options={})
-    options.reverse_merge! :method => :put, :class => "resume action", :title => "resume", :name => "resume"    
+    options.reverse_merge! :method => :put, :class => "resume action", :title => "resume", :name => "resume", :accesskey => "r"    
+    add_access_key_to_title options    
     link_to options[:name], task_resume_path(task), options
   end
   
@@ -19,4 +22,8 @@ module ApplicationHelper
     content_tag :span, task.status, :class => task.status
   end
 
+  def add_access_key_to_title(options)
+    options[:title] += " (#{options[:accesskey]})" if options[:accesskey]
+  end
+  
 end
